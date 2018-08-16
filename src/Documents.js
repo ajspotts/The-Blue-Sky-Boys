a62d8e9d20d1faca12596ed92108d25a96f89145
Alexander <ajspottswood@gmail.com>
2018-08-16 17:30:20 -0400

    added captions to 30s, docs, & instruments

--- src/Documents.js
+++ src/Documents.js
@@ -3,18 +3,16 @@ import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 
 const photos = [
-  { src: 'https://s20.postimg.cc/gh4picqod/001.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/asyerbwkt/005.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/r3yinje6l/LR_acceptance_51.jpg', caption: 'The envelope that contained Bill’s letter of acceptance to Lenoir Rhyne College, 1951', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/m5b094kp9/LR_Transcript_1956.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/x4w7kw3gd/Bill_Bolick_High_School_Diploma.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/n7l6rp63h/Blue_Bird_Records_Add_001.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/m5b092fjh/Debate_award_1953.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/cxirsdg71/Debate_Award-2.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/nkckxue2l/Debating_Award_1953.jpg', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/nx3z3yr6l/Illini_Concert.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/epbqnauf1/RCA_Promotions.jpg', caption: '', width: 0.2, height: 0.2 },
-  { src: 'https://s20.postimg.cc/zc0g88nzx/Ford_purchase_1-46.jpg', caption: '', width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/gh4picqod/001.jpg', caption: 'RCA promotion notice, 1947', width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/asyerbwkt/005.jpg', caption: 'RCA Bluebird catalog, 1936', width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/r3yinje6l/LR_acceptance_51.jpg', caption: "Bill's college acceptance letter", width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/m5b094kp9/LR_Transcript_1956.jpg', caption: "Bill's college transcript, 1956", width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/x4w7kw3gd/Bill_Bolick_High_School_Diploma.jpg', caption: "Bill's high school diploma, 1933", width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/n7l6rp63h/Blue_Bird_Records_Add_001.jpg', caption: 'RCA Bluebird catalog, 1936', width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/m5b092fjh/Debate_award_1953.jpg', caption: '"Extemporaneous speaking" award, 1953', width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/cxirsdg71/Debate_Award-2.jpg', caption: 'Debating award, 1953', width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/nx3z3yr6l/Illini_Concert.jpg', caption: "Poster,  Blue Sky Boys' first appearance since their 1951 retirement", width: 0.2, height: 0.2 },
+  { src: 'https://s20.postimg.cc/epbqnauf1/RCA_Promotions.jpg', caption: 'RCA promotion flyer, 1948', width: 0.2, height: 0.2 },
 ];
 
 export class Documents extends React.Component {
